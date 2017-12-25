306084fc1106d88ff8da1ff9c51d3c543ed2517a
GrosPoulet\Vincent <v.labreche@gmail.com>
2017-12-25 03:03:32 -0500

    no message

--- DEDU2.ino
+++ DEDU2.ino
@@ -142,11 +142,10 @@ void setup()
 //Setup complete.  MAIN loop.
 void loop() {
 
-    goto FFA;
+
 
 start:
-
-  
+    
   TurnOffAllRedLights();
   delay(500);
 
@@ -383,7 +382,8 @@ MARQUEURHONTE:
 
 FFA:
 
-  ChansonDEDU(4);
+  ChansonDEDU(1);
+  
   for (int e = 1; e <= 35; e++) {
 
     r = 154 - random(20);
@@ -511,92 +511,6 @@ void ChansonDEDU(double facteur)
   {
     PlayNote(Tone_Pin,Notes[i],Play_Time[Play_Time_Index[i]],Wait_Time[Wait_Time_Index[i]]);
   }
-
-/*
- * 
- * /*
-NEW
-NOTE  g3  g3  g3  b3  b3  c4  c4  F4  b3  g3  g3  d4  b3  g3  e4  d4  b3  c4
-TIME  0   0   0   1   1   1   1   2   0   0   0   0   0   0   2   0   0   2
-WAIT  0   1   0   1   1   1   1   2   0   1   0   1   0   1+1 2+1 1   0   2
-
-  int Note_G3 = 196;
-  int Note_A3 = 220;
-  int Note_B3 = 247;
-  int Note_C4 = 261;
-  int Note_D4 = 294;
-  int Note_E4 = 330;
-  int Note_F4 = 349;
-  
-  double joue_croche = 150 / facteur;
-  double delay_croche = 100 / facteur;
-  
-  double joue_noire = 2*joue_croche;
-  double delay_noire = 2*delay_croche;
-  
-  double joue_blanche = 4*joue_croche;
-  double delay_blanche = 2*delay_croche;
-
-
-
-  tone(Tone_Pin, Note_G3, joue_croche);
-  delay(joue_croche);
-  delay(delay_croche);
-  tone(Tone_Pin, Note_G3, joue_croche);
-  delay(joue_croche);
-  delay(delay_noire);
-  tone(Tone_Pin, Note_G3, joue_croche);
-  delay(joue_croche);
-  delay(delay_croche);
-  tone(Tone_Pin, Note_B3, joue_noire);
-  delay(joue_noire);
-  delay(delay_noire);
-  tone(Tone_Pin, Note_B3, joue_noire);
-  delay(joue_noire);
-  delay(delay_noire);
-  tone(Tone_Pin, Note_C4, joue_noire);
-  delay(joue_noire);
-  delay(delay_noire);
-  tone(Tone_Pin, Note_C4, joue_noire);
-  delay(joue_noire);
-  delay(delay_noire);
-  tone(Tone_Pin, Note_F4, joue_blanche);
-  delay(joue_blanche);
-  delay(delay_blanche);
-  tone(Tone_Pin, Note_B3, joue_croche);
-  delay(joue_croche);
-  delay(delay_croche);
-  tone(Tone_Pin, Note_G3, joue_croche);
-  delay(joue_croche);
-  delay(delay_noire);
-  tone(Tone_Pin, Note_G3, joue_croche);
-  delay(joue_croche);
-  delay(delay_croche);
-  tone(Tone_Pin, Note_D4, joue_croche);
-  delay(joue_croche);
-  delay(delay_noire);
-  tone(Tone_Pin, Note_B3, joue_croche);
-  delay(joue_croche);
-  delay(delay_croche);
-  tone(Tone_Pin, Note_G3, joue_croche);
-  delay(joue_croche);
-  delay(delay_noire);
-  delay(delay_noire);
-  tone(Tone_Pin, Note_E4, joue_blanche);
-  delay(joue_blanche);
-  delay(delay_blanche);
-  delay(delay_noire);
-  tone(Tone_Pin, Note_D4, joue_croche);
-  delay(joue_croche);
-  delay(delay_noire);
-  tone(Tone_Pin, Note_B3, joue_croche);
-  delay(joue_croche);
-  delay(delay_croche);
-  tone(Tone_Pin, Note_C4, joue_blanche);
-  delay(joue_blanche);
-  delay(delay_blanche);
-
-  */
 }
 
 //Turn off all the reds
