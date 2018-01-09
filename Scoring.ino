40f5473fe94fdc7b32d4a75efd4df563c3092b81
GrosPoulet\Vincent <v.labreche@gmail.com>
2018-01-09 10:55:00 -0500

    Small tweak to scoring function. Add logs.

--- Scoring.ino
+++ Scoring.ino
@@ -1,9 +1,35 @@
 float RelativeError(float TestValue,float TrueValue)
 {
   float Score;
-  Score = 1 - (  abs( TrueValue-TestValue )  /  ( 1+TrueValue) );
+
+
+  
+  if (TrueValue !=0)
+  {
+    Score = 1 - (  abs( TrueValue-TestValue )  /  ( TrueValue) );
+  }
+  else
+  {
+    Score=0;
+  }
+
+  
   if (Score < 0) Score = 0;
 
+/*
+  LOG_GENERAL("\n");
+  LOG_GENERAL("\n");
+  LOG_GENERAL("Test:");
+  LOG_GENERAL(TestValue);
+  LOG_GENERAL("\n");
+  LOG_GENERAL("True:");
+  LOG_GENERAL(TrueValue);
+  LOG_GENERAL("\n");
+  LOG_GENERAL("Score:");
+  LOG_GENERAL(Score);
+  LOG_GENERAL("\n");
+  LOG_GENERAL("\n");
+*/
   return Score;
 }
 
